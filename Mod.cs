219fd5cb3a81a391e0204e0f82331c878cf9f63d
unknown-user <unknown-user@fe80::5929:37ac:1d7b:41d6%26>
2015-05-24 14:18:24 -0400

    Initial check-in of module EnhancedRoadHeights

---
+++ Mod.cs
@@ -0,0 +1,27 @@
+﻿
+using ICities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EnhancedRoadHeight
+{
+    public class ToolAnarchyMod : IUserMod
+    {
+        public string Name
+        {
+            get
+            {
+                return "Enhanced Road Heights";
+            }
+        }
+        public string Description
+        {
+            get
+            {
+                return "Changes road heights from 12m to 3m";
+            }
+        }
+    }
+}
